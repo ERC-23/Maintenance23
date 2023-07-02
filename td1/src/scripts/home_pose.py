2a97bb7239bae5ce4571d9b5f0226ea2b1dc7ab9
KhaledEssam24 <khaledessam24120@gmail.com>
2023-07-02 03:51:10 +0300

    add constraints

--- td1/src/scripts/home_pose.py
+++ td1/src/scripts/home_pose.py
@@ -8,6 +8,7 @@ import time
 import subprocess
 from std_msgs.msg import Bool
 from math import pi
+from moveit_msgs.msg import Constraints,JointConstraint
 
 
 #########################   Node Initialization     #########################
@@ -40,6 +41,8 @@ display_trajectory_publisher = rospy.Publisher(
             queue_size=20,
         )
 
+home=[0,-2*pi/3,5*pi/9,pi/9,pi/2,-pi/2]
+
 #########################   Functions Implementation  #########################
 
 def grip_close():
@@ -66,14 +69,48 @@ def move_rotational(joint_goal) :
   
   move_group.go(joint_goal, wait=True)
 
+####################### Constrains #######################
+def Set_constraints():
+    
+    joints_con = Constraints()
+    joints_con.name = 'goal_constraints'
+
+    elbow_con = JointConstraint()
+    elbow_con.joint_name = 'elbow_joint'
+    elbow_con.position = home[2]
+    elbow_con.tolerance_above = 1
+    elbow_con.tolerance_below = 1
+    elbow_con.weight = 1.0
+
+    joints_con.joint_constraints.append(elbow_con)
+
+    wrist3_con = JointConstraint()
+    wrist3_con.joint_name = "wrist_3_joint"
+    wrist3_con.position   =  home[5]
+    wrist3_con.tolerance_above = 0.2
+    wrist3_con.tolerance_below = 0.2
+    wrist3_con.weight = 1.0
+
+    joints_con.joint_constraints.append(wrist3_con)
+
+    base_con = JointConstraint()
+    base_con.joint_name = "shoulder_pan_joint"
+    base_con.position   =  home[0]
+    base_con.tolerance_above = 1.8
+    base_con.tolerance_below = 1.8
+    base_con.weight = 0.8
+
+    joints_con.joint_constraints.append(base_con)
+
+    move_group.set_path_constraints(joints_con)
+
 
 if __name__ == '__main__':
     try:
 
         print("######## Objective 10 is started #######")
-
-        init_pos=[0,-2*pi/3,5*pi/9,pi/9,pi/2,-pi/2]
-        move_rotational(init_pos)
+        Set_constraints()
+        move_rotational(home)
         start_pose = move_group.get_current_pose().pose
         
         grip_open()
